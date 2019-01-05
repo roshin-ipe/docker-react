67a3a61ca0ac2e28018102e3a984351d94cacc0f
roshin-ipe <roshin.ipe@gmail.com>
2019-01-05 13:45:15 +0530

    first commit

---
+++ build/precache-manifest.0ffbeda81676f099b330d4fcbe27afc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "666e7c9f1b7453e82fe3",
+    "url": "/static/css/main.cfa1799d.chunk.css"
+  },
+  {
+    "revision": "666e7c9f1b7453e82fe3",
+    "url": "/static/js/main.666e7c9f.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "86bdfbaee393927e178654e015640feb",
+    "url": "/index.html"
+  }
+];
